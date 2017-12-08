9b000a5af0870f72e03d3576047ad52a1da2b0b8
robsarno <roberto.arnoldi.1999@gmail.com>
2017-12-08 16:11:23 +0100

    right URL

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $BASE_WEBSITE="https://api.foursquare.com/v2/venues/";
 $CLIENT_ID="TLP33XZ2XLGU2SKN50SPMU40MAXDUELATV45VPPPGRLSQEQE";
 $CLIENT_SECRET="STCQN42LRMGGPTOGG5UQGNXACRDGWWYSNOYAL2PKJPMFYBVU";
 
+$URL=$BASE_WEBSITE.'search?client_id='.$CLIENT_ID.'&client_secret='.$CLIENT_SECRET.'&v='.date("Ymd").'&near=Bergamo';
 
+echo $URL;
 
+https://api.foursquare.com/v2/venues/categories?client_id={0}&client_secret={1}&v={2}
 ?>
