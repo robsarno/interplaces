1924158b043a8d5298cd7f0eb7ef4878e8498960
robsarno <roberto.arnoldi.1999@gmail.com>
2017-12-09 20:59:02 +0100

    added general navbar

---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar sticky-top navbar-dark bg-primary justify-content-between">
+  <div class="container">
+    <a class="navbar-brand text-white">InterPlaces</a>
+    <form class="form-inline my-2 my-lg-0" method="get" action="/interplaces/search.php">
+      <input class="form-control mr-sm-3" placeholder="Cerco... (es. pizzeria)" aria-label="Cosa cerchi?" name="cosa">
+      <input class="form-control mr-sm-3" placeholder="Vicino a... (es. Bergamo)" aria-label="Vicino a?" name="dove">
+      <button class="btn btn btn-outline-light my-2 my-sm-0" type="submit">Cerca</button>
+    </form>
+  </div>
+</nav>
