398d1ffac9f45f522ddf69ff438e12baf5289e76
robsarno <roberto.arnoldi.1999@gmail.com>
2017-12-09 21:05:09 +0100

    added hide form on mobile

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar sticky-top navbar-dark bg-primary justify-content-between">
   <div class="container">
     <a class="navbar-brand text-white" href="http://interplaces.robertoarnoldi.altervista.org">InterPlaces</a>
-    <form class="form-inline my-2 my-lg-0" method="get" action="/interplaces/search.php">
+    <form class="form-inline my-2 my-lg-0 d-none d-md-block" method="get" action="/interplaces/search.php">
       <input class="form-control mr-sm-3" placeholder="Cerco... (es. pizzeria)" aria-label="Cosa cerchi?" name="cosa">
       <input class="form-control mr-sm-3" placeholder="Vicino a... (es. Bergamo)" aria-label="Vicino a?" name="dove">
       <button class="btn btn btn-outline-light my-2 my-sm-0" type="submit">Cerca</button>
