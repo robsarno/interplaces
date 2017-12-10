70798034388c185a60079747295717af460fd799
robsarno <roberto.arnoldi.1999@gmail.com>
2017-12-10 11:51:50 +0100

    bold title

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar sticky-top navbar-dark bg-primary justify-content-between" style="-webkit-box-shadow: 0px 14px 34px -6px rgba(0,0,0,0.75);-moz-box-shadow: 0px 14px 34px -6px rgba(0,0,0,0.75);box-shadow: 0px 14px 34px -6px rgba(0,0,0,0.75);">
   <div class="container">
-    <a class="navbar-brand text-white" href="http://interplaces.robertoarnoldi.altervista.org">InterPlaces</a>
+    <a class="navbar-brand text-white" href="http://interplaces.robertoarnoldi.altervista.org"><b>InterPlaces</b></a>
     <form class="form-inline my-2 my-lg-0 d-none d-md-block" method="get" action="/interplaces/search.php">
       <input class="form-control mr-sm-3" placeholder="Cerco... (es. pizzeria)" aria-label="Cosa cerchi?" name="cosa">
       <input class="form-control mr-sm-3" placeholder="Vicino a... (es. Bergamo)" aria-label="Vicino a?" name="dove">
