addd325dbcb4ebfe6d3f3a3225a1391e2da0a41b
robsarno <roberto.arnoldi.1999@gmail.com>
2017-12-10 11:37:22 +0100

    added shadow

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-dark bg-primary justify-content-between">
+<nav class="navbar sticky-top navbar-dark bg-primary justify-content-between" style="-webkit-box-shadow: 0px 14px 34px -6px rgba(0,0,0,0.75);-moz-box-shadow: 0px 14px 34px -6px rgba(0,0,0,0.75);box-shadow: 0px 14px 34px -6px rgba(0,0,0,0.75);">
   <div class="container">
     <a class="navbar-brand text-white" href="http://interplaces.robertoarnoldi.altervista.org">InterPlaces</a>
     <form class="form-inline my-2 my-lg-0 d-none d-md-block" method="get" action="/interplaces/search.php">
